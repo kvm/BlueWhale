bd5d4812db4ce0399e90d567793cfc186e1f2537
Ankur Khurana <ankurkhurana@nsitonline.in>
2013-01-12 21:06:25 +0530

    CSUser Table DDL

---
+++ BackEnd/DDL/CSuser.sql
@@ -0,0 +1,9 @@
+Create Table CSuser(
+ID LONG PRIMARY KEY,
+ServiceUserName VARCHAR(100),
+ServiceUserID LONG NOT NULL,
+ServiceType INT NOT NULL,
+IdeOneID INT NOT NULL,
+FirstName Varchar(100),
+LastName VarChar(100)
+)
